40e80297e34b003f621d4607696e8fb803525768
Bibin A <bibinolavanna@gmail.com>
2023-01-03 14:08:14 +0530

    new

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../Model/userModel');
+const User = require("../Model/userModel");
 
 //User signup
 const InsertUser = async (req, res) => {
@@ -13,78 +13,90 @@ const InsertUser = async (req, res) => {
     const email = req.body.email;
     const user = await User.findOne({ email: email });
     if (email == user.email) {
-      res.render('../Views/User/usersignup.ejs',{error:"E-mail already exist"});
+      res.render("../Views/User/usersignup.ejs",{error: "E-mail already exist"});
     }
   } catch (error) {
-    const UserData= user1.save();
-    res.render('../Views/User/usersignup.ejs',{success:"Registration success please login"});
+    const UserData = user1.save();
+    res.render("../Views/User/usersignup.ejs", {
+      success: "Registration success please login",
+    });
     console.log(error);
   }
 };
 
 let user;
-const UserVerification=async (req,res) =>{
-    try {
-        const email=req.body.email;
-        const password=req.body.password;
-        user=await User.findOne({email:email});
+const UserVerification = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    user = await User.findOne({ email: email });
 
-        if(user){
-            if(email===user.email && password===user.password){
-                req.session.user=req.body.email;
-                console.log("session created");
-                res.redirect('/userhome');
-            }else{
-                res.render('../Views/User/userlogin.ejs',{wrong:"Invalid Email or Password"});
-            }
-        }else{
-            res.render('../Views/User/userlogin.ejs',{wrong:"User not found"})
-        }
-    } catch (error) {
-        console.log(error.message);
-        res.render('usersignup',{wrong:"User not found"})
+    if (user) {
+      if (email === user.email && password === user.password) {
+        req.session.user = req.body.email;
+        console.log("session created");
+        res.redirect("/userhome");
+      } else {
+        res.render("../Views/User/userlogin.ejs", {
+          wrong: "Invalid Email or Password",
+        });
+      }
+    } else {
+      res.render("../Views/User/userlogin.ejs", { wrong: "User not found" });
     }
+  } catch (error) {
+    console.log(error.message);
+    res.render("usersignup", { wrong: "User not found" });
+  }
 };
 
 //User login page
 const UserLogin = async (req, res) => {
-    if(req.session.user){
-        res.redirect('/userhome');
-    }else{
-        try {
-            res.render("../Views/User/userlogin.ejs");
-        } catch (error) {
-            console.log(error.message);
-        }
+  if (req.session.user) {
+    res.redirect("/userhome");
+  } else {
+    try {
+      res.render("../Views/User/userlogin.ejs");
+    } catch (error) {
+      console.log(error.message);
     }
-  };
+  }
+};
 
 //User signup page
 const UserSignup = (req, res) => {
-    if(req.session.user){
-        res.redirect('/userhome')
-    }else{
-        res.render("../Views/User/usersignup.ejs");
-    }
+  if (req.session.user) {
+    res.redirect("/userhome");
+  } else {
+    res.render("../Views/User/usersignup.ejs");
+  }
 };
-
-
 
 //User home page
-const UserHome =async (req, res) => {
-    if(req.session.user){
-        res.render('../Views/User/userhome.ejs',{user});
-    }else{
-        res.redirect('/');
+const UserHome = async (req, res) => {
+  try {
+    if (req.session.user) {
+      const UserDbData = await User.findOne({ email: req.session.user });
+      if (UserDbData) {
+        res.render('../Views/User/userhome.ejs',{ user });
+      } else {
+        req.session.destroy();
+        res.redirect("/");
+      }
+    } else {
+      res.redirect("/");
     }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const UserLogout=async(req,res)=>{
-    req.session.destroy();
-    console.log("session destroyed");
-    res.redirect('/');
-    res.end();
-}
+const UserLogout = async (req, res) => {
+  req.session.destroy();
+  console.log("session destroyed");
+  res.redirect("/");
+  res.end();
+};
 
 module.exports = {
   UserLogin,
@@ -92,5 +104,5 @@ module.exports = {
   UserHome,
   InsertUser,
   UserVerification,
-  UserLogout
+  UserLogout,
 };
